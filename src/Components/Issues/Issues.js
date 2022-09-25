3362aa6df76c9521fbc30b394e70a4374f43dbcd
DudsTheKid3756 <curlyq3756@gmail.com>
2022-09-24 21:26:24 -0400

    added mui card

--- src/Components/Issues/Issues.js
+++ src/Components/Issues/Issues.js
@@ -1,13 +1,18 @@
 import './Issues.component.css';
+import { Card, CardContent, Typography } from '@mui/material';
 
-const Issues = () => {
+function Issues() {
   const name = 'Tyler';
 
   return (
-    <div className="header">
-      <h1>Hello {name}! Welcome!</h1>
+    <div className="container">
+      <Card className="card" sx={{ maxWidth: '50%', display: 'inline-block' }}>
+        <CardContent className="content" sx={{ backgroundColor: 'lightgray' }}>
+          <Typography>Hi {name}!</Typography>
+        </CardContent>
+      </Card>
     </div>
   );
-};
+}
 
 export default Issues;
